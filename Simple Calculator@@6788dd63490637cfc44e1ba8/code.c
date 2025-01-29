e9e3cc0d233675e78077f5fe42dbfebd622fb7f2
anshul13-hub <195144856+anshul13-hub@users.noreply.github.com>
2025-01-29 09:37:28 +0530

    Solution to Simple Calculator

---
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    char op;
+    scanf("%d %d ",&a,&b);
+    switch(op)
+    {
+        case '+' :
+        printf("%d",a+b);
+        break;
+    }
+    
+}
