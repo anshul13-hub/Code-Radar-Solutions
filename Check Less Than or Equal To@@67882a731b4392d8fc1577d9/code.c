5a9078eb88dc06ca6e660ecf51454424ea6e0ae5
anshul13-hub <195144856+anshul13-hub@users.noreply.github.com>
2025-02-04 12:30:13 +0530

    Solution to Check Less Than or Equal To

--- Check Less Than or Equal To@@67882a731b4392d8fc1577d9/code.c
+++ Check Less Than or Equal To@@67882a731b4392d8fc1577d9/code.c
@@ -1,18 +1,15 @@
 #include <stdio.h>
-int main()
+int main ()
 {
     int a,b;
-    scanf("%d %d",&a,&b)
+    scanf("%d %d",&a,&b);
+    if(a<=b)
     {
-        if(a>=b)
-        {
-            printf("True");
-        }
-        else
-        {
-            printf("False");
-        }
-
+        printf("True");
+    }
+    else
+    {
+        printf("False");
     }
     return 0;
 }
