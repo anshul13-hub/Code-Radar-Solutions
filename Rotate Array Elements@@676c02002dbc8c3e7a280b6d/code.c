f07e96535291d19586395798111781024f7d9905
Anshul lal <195144856+anshul13-hub@users.noreply.github.com>
2025-03-07 13:54:38 +0530

    Solution to Rotate Array Elements

--- Rotate Array Elements@@676c02002dbc8c3e7a280b6d/code.c
+++ Rotate Array Elements@@676c02002dbc8c3e7a280b6d/code.c
@@ -1,43 +1,36 @@
 #include <stdio.h>
+int main ()
 
-int main() {
-    int N, K;
-
-    // Read the number of elements and the number of positions to rotate
+{
+    int N,K;
     scanf("%d", &N);
     int arr[N];
-    
-    // Read the elements of the array
-    for (int i = 0; i < N; i++) {
-        scanf("%d", &arr[i]);
+    printf("enter elements of array");
+    for(int i = 0;i<N;i++)
+    {
+        scanf("%d",&arr[i]);
+        
     }
-
-    // Read the value of K and normalize it (K % N to handle cases where K > N)
     scanf("%d", &K);
-    K = K % N;
-
-    // Step 1: Reverse the entire array
-    for (int i = 0; i < N / 2; i++) {
+    K = K % N ;
+    for(int i = 0;i<N/2;i++){
         int temp = arr[i];
-        arr[i] = arr[N - 1 - i];
-        arr[N - 1 - i] = temp;
-    }
+        arr[i] = arr[N-1-i];
+        arr[N-1-i] = temp ;
 
-    // Step 2: Reverse the first K elements
-    for (int i = 0; i < K / 2; i++) {
+    }
+    for(int i = 0;i<K/2;i++){
         int temp = arr[i];
-        arr[i] = arr[K - 1 - i];
-        arr[K - 1 - i] = temp;
-    }
+        arr[i] = arr[K-1-i];
+        arr[K-1-i] = temp ;
 
-    // Step 3: Reverse the remaining N - K elements
-    for (int i = K; i < (N + K) / 2; i++) {
+    }
+    for(int i = K;i<(N+K)/2;i++){
         int temp = arr[i];
-        arr[i] = arr[N - 1 - (i - K)];
-        arr[N - 1 - (i - K)] = temp;
-    }
+        arr[i] = arr[N-1-(i-K)];
+        arr[N-1-(i-K)] = temp ;
 
-    // Print the rotated array
+    }
     for (int i = 0; i < N; i++) {
         printf("%d ", arr[i]);
     }
@@ -45,3 +38,6 @@ int main() {
 
     return 0;
 }
+    
+    
+
