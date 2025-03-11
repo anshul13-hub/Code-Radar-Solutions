9200629b672b4717b4675d2c908e8b905ed87610
Anshul lal <195144856+anshul13-hub@users.noreply.github.com>
2025-03-11 13:17:48 +0530

    Solution to Find the Index of a Target Element in an Array

--- Find the Index of a Target Element in an Array@@676c02002dbc8c3e7a280b77/code.c
+++ Find the Index of a Target Element in an Array@@676c02002dbc8c3e7a280b77/code.c
@@ -1,36 +1,22 @@
 #include <stdio.h>
-
-int main() {
-    int N;
-
-    // Input the size of the array
-    scanf("%d", &N);
-
-    int arr[N];  // Declare an array of size N
-
+int main ()
+{
+    int N ;
+    scanf("%d",&N);
+    int arr[N];
     int T;
-
-    // Input the target element to find
-    scanf("%d", &T);
-
-    // Input the array elements
-    for (int i = 0; i < N; i++) {
-        scanf("%d", &arr[i]);  // Fill the array with input elements
+    scanf("%d",%T);
+    for(int i = 0;i<N;i++)
+    {
+        scanf("%d",&arr[i]);
     }
-
-    int found = 0;  // To check if any index was found
-
-    // Search for the target element and print all indices where it occurs
-    for (int i = 0; i < N; i++) {
-        if (arr[i] == T) {  // Check if the element matches the target
-            printf("%d ", i);  // Print the index of the occurrence
-            found = 1;  // Mark that we found at least one occurrence
+    int foundindx= 0;
+    for(int i = 0;i<N;i++)
+    {
+        if(arr[i]==T)
+        {
+            foundindx = i;
         }
+        
     }
-    printf("%d ",i);
-
-    // If no occurrences of the target element were found, print -1
-    
-
-    return 0;
 }
