201a955bd8e41784409114edcdbe24e47b91a62e
Anshul lal <195144856+anshul13-hub@users.noreply.github.com>
2025-03-18 16:33:31 +0530

    Solution to Find the Median of an Array

--- Find the Median of an Array@@676c02002dbc8c3e7a280b8c/code.c
+++ Find the Median of an Array@@676c02002dbc8c3e7a280b8c/code.c
@@ -1,34 +1,35 @@
 #include <stdio.h>
-int main ()
-{
-    int N;
-    scanf("%d",&N);
-    int arr[N];
-    for(int i = 0;i<N;i++)
-    {
-        scanf("%d",&arr[i]);
-    }
-    for(int i = 0;i<N-1;i++)
-    {
-        for(int j = i+1;j<N;j++)
-        {
-            if(arr[i]>arr[j])
-            {
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
 
+int main() {
+    int N;
+
+    scanf("%d", &N);
+
+    int arr[N];
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int temp;
+
+    for (int i = 0; i < N - 1; i++) {
+        for (int j = 0; j < N - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
         }
     }
-    if(N%2==1)
-    {
-        printf("%d\n",arr[N/2]);
-    }
-    else
-    {
-        int median = (arr[N/2-1]+arr[N/2]/2);
-        printf("%d\n",median);
-    }
 
+    // Finding the median
+    float median;
+    if (N % 2 == 0) {
+        median = (arr[(N / 2) - 1] + arr[N / 2]) / 2.0;
+    } else {
+        median = arr[N / 2];
+    }
+    printf("%.2f\n", median);
+
+    return 0;
 }
